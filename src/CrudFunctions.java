bb418baaf607d4443e9be4a96525091bda092b76
Nikita Shtreys <n.shtreys@vk.team>
2024-09-26 23:32:45 +0300

    format code

--- src/CrudFunctions.java
+++ src/CrudFunctions.java
@@ -2,7 +2,10 @@ import java.util.Optional;
 
 public interface CrudFunctions<K, V> {
     void put(K key, V value);
+
     Optional<V> get(K key);
+
     boolean delete(K key);
+
     boolean containsKey(K key);
 }
